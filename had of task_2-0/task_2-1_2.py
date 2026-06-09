43e64216ce8c34e243d6dad0f594694b7bd904d0
Dr. Nazgul <bulanov.mm@edu.spbstu.ru>
2026-06-09 15:43:24 +0300

    Add files via upload

--- had of task_2-0/task_2-1_2.py
+++ had of task_2-0/task_2-1_2.py
@@ -1,4 +1,4 @@
 python_version = "3.10"
 lang = "Python"
-print("Language:", {lang}, end="\n")
-print("Version:", {python_version}, sep="\t")
+print(f"Language:", lang, end="\n")
+print(f"   Version:", python_version, )
